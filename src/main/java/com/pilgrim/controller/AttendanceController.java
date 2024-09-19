b5ef524d58ad1a49683d48f3085656c379a096f2
Joao Cipriano <joaolucassalvatore@gmail.com>
2024-09-19 20:15:02 -0300

    chore: remove dynamo repository from AttendanceController

--- src/main/java/com/pilgrim/controller/AttendanceController.java
+++ src/main/java/com/pilgrim/controller/AttendanceController.java
@@ -2,7 +2,6 @@ package com.pilgrim.controller;
 
 import com.pilgrim.controller.model.AttendanceRequest;
 import com.pilgrim.controller.model.AttendanceResponse;
-import com.pilgrim.infrastructure.dynamodb.DynamoRepository;
 import com.pilgrim.repository.AttendanceEntity;
 import com.pilgrim.repository.AttendanceRepository;
 import io.micronaut.http.annotation.Body;
@@ -22,7 +21,6 @@ import java.time.LocalDateTime;
 public class AttendanceController {
 
     private final AttendanceRepository attendanceRepository;
-    private final DynamoRepository dynamoRepository;
 
     @Post
     public AttendanceResponse save(@Body AttendanceRequest request) {
@@ -35,8 +33,6 @@ public class AttendanceController {
 
     @Get("/{id}")
     public AttendanceResponse get(@PathVariable String id) {
-        var membership = dynamoRepository.getItem("PREMIUM", 0);
-        log.info("MEMBERSHIP: {}", membership);
         var entity = attendanceRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException(id, AttendanceEntity.class));
         return new AttendanceResponse(entity.getId(), entity.getName(), entity.getCreatedAt());
