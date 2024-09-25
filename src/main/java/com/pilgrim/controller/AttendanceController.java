7191fe36ff28804fcd2df48b6fe78b956bf54153
Joao Cipriano <joaolucassalvatore@gmail.com>
2024-09-25 20:26:01 -0300

    chore: add dynamo repository in AttendanceController

--- src/main/java/com/pilgrim/controller/AttendanceController.java
+++ src/main/java/com/pilgrim/controller/AttendanceController.java
@@ -2,6 +2,7 @@ package com.pilgrim.controller;
 
 import com.pilgrim.controller.model.AttendanceRequest;
 import com.pilgrim.controller.model.AttendanceResponse;
+import com.pilgrim.infrastructure.dynamodb.DynamoRepository;
 import com.pilgrim.repository.AttendanceEntity;
 import com.pilgrim.repository.AttendanceRepository;
 import io.micronaut.http.annotation.Body;
@@ -21,6 +22,7 @@ import java.time.LocalDateTime;
 public class AttendanceController {
 
     private final AttendanceRepository attendanceRepository;
+    private final DynamoRepository dynamoRepository;
 
     @Post
     public AttendanceResponse save(@Body AttendanceRequest request) {
@@ -33,6 +35,8 @@ public class AttendanceController {
 
     @Get("/{id}")
     public AttendanceResponse get(@PathVariable String id) {
+        var membership = dynamoRepository.getItem("PREMIUM", 0);
+        log.info("MEMBERSHIP: {}", membership);
         var entity = attendanceRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException(id, AttendanceEntity.class));
         return new AttendanceResponse(entity.getId(), entity.getName(), entity.getCreatedAt());
