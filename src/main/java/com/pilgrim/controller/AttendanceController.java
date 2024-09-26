c4232d4c2d61874484f4d56bcd5eb98e27780a98
Joao Cipriano <joaolucassalvatore@gmail.com>
2024-09-25 21:04:15 -0300

    chore: remove dynamo repository from AttendanceController

--- src/main/java/com/pilgrim/controller/AttendanceController.java
+++ src/main/java/com/pilgrim/controller/AttendanceController.java
@@ -2,7 +2,6 @@ package com.pilgrim.controller;
 
 import com.pilgrim.controller.model.AttendanceRequest;
 import com.pilgrim.controller.model.AttendanceResponse;
-import com.pilgrim.infrastructure.dynamodb.DynamoRepository;
 import com.pilgrim.repository.AttendanceEntity;
 import com.pilgrim.repository.AttendanceRepository;
 import io.micronaut.http.annotation.Body;
@@ -11,18 +10,15 @@ import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
 import io.micronaut.http.annotation.Post;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.hibernate.ObjectNotFoundException;
 
 import java.time.LocalDateTime;
 
-@Slf4j
 @Controller("/attendances")
 @RequiredArgsConstructor
 public class AttendanceController {
 
     private final AttendanceRepository attendanceRepository;
-    private final DynamoRepository dynamoRepository;
 
     @Post
     public AttendanceResponse save(@Body AttendanceRequest request) {
@@ -35,8 +31,6 @@ public class AttendanceController {
 
     @Get("/{id}")
     public AttendanceResponse get(@PathVariable String id) {
-        var membership = dynamoRepository.getItem("PREMIUM", 0);
-        log.info("MEMBERSHIP: {}", membership);
         var entity = attendanceRepository.findById(id)
                 .orElseThrow(() -> new ObjectNotFoundException(id, AttendanceEntity.class));
         return new AttendanceResponse(entity.getId(), entity.getName(), entity.getCreatedAt());
